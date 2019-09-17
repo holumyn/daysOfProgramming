6ddea138b6b15f22249c5274f776d57e71265548
Chigozie <chygoz2@gmail.com>
2019-09-17 22:12:41 +0100

    Added tests

--- javascript/test/Question2.test.js
+++ javascript/test/Question2.test.js
@@ -1,23 +1,23 @@
 const chai = require('chai');
 const assert = chai.assert;
-const beautifulDays = require('../question2')
+const utopianTree = require('../question2')
 
 describe('Question 2', () => {
-  it ('Can get the number of beautiful days for test case 1', done => {
-    let result = beautifulDays(20, 23, 6)
+  it ('Can get the tree height for test case 1', done => {
+    let result = utopianTree(0)
+    assert.equal(result, 1)
+    done()
+  })
+
+  it ('Can get the tree height for test case 2', done => {
+    let result = utopianTree(1)
     assert.equal(result, 2)
     done()
   })
 
-  it ('Can get the number of beautiful days for test case 2', done => {
-    let result = beautifulDays(13, 45, 3)
-    assert.equal(result, 33)
-    done()
-  })
-
-  it ('Can get the number of beautiful days for test case 3', done => {
-    let result = beautifulDays(1, 123456, 13)
-    assert.equal(result, 9657)
+  it ('Can get the tree height for test case 3', done => {
+    let result = utopianTree(4)
+    assert.equal(result, 7)
     done()
   })
 })
